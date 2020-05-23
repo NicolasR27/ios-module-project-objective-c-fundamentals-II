e51a78c5feae6be64e9f90ddc4b0bc4096e8260d
Nicolas Rios <nickrios33@yahoo.com>
2020-05-23 02:09:05 -0700

    added the LISITimedTask. h and .M

---
+++ TimeTracker/Model/LSITimedTask.h
@@ -0,0 +1,17 @@
+//
+//  LSITimedTask.h
+//  TimeTracker
+//
+//  Created by Nicolas Rios on 5/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LSITimedTask : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TimeTracker/Model/LSITimedTask.m
@@ -0,0 +1,13 @@
+//
+//  LSITimedTask.m
+//  TimeTracker
+//
+//  Created by Nicolas Rios on 5/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+#import "LSITimedTask.h"
+
+@implementation LSITimedTask
+
+@end
